123abe7e5eb2882ff98f8b5c4e485517a9e3f19d
davidrey <david_sardy@hotmail.com>
2025-02-07 14:48:07 +0100

    Exercise31 Resolto

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,34 @@
 package com.campusdual.classroom;
 
+import com.sun.tools.javac.Main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
+        String rutaDelArchivo = "lorem.txt";
 
+
+        //classLoader obten recurso como InputStream
+        try (InputStream inputStream = Main.class.getClassLoader().getResourceAsStream(rutaDelArchivo);
+
+             BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+
+            if (inputStream == null) {//Verifico si se encontra o archivo
+                System.out.println("Archivo non encontrado: " + rutaDelArchivo);
+            }
+
+            String lineaPorLinea;
+            while ((lineaPorLinea = br.readLine()) != null) {//leo o archivo línea por línea
+                System.out.println(lineaPorLinea);
+            }
+        } catch (IOException e) {
+            System.out.println("Error ao leer o archivo: " + e.getMessage());
+        }
     }
 }
+
